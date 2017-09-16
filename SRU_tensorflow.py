0724840733a77bb23769a7675825c585ef741478
desire2020 <desire2020@126.com>
2017-09-16 17:43:08 +0800

    bug fix.

--- SRU_tensorflow.py
+++ SRU_tensorflow.py
@@ -20,6 +20,7 @@ class SRUCell(RNNCell):
 
         self.U = tf.Variable(init_matrix([self.hidden_dim, self.hidden_dim]))
 
+
     @property
     def state_size(self):
         return self.hidden_dim
@@ -31,9 +32,9 @@ class SRUCell(RNNCell):
     def __call__(self, inputs, state, scope=None):
         with vs.variable_scope(scope or type(self).__name__):
             if self.state_is_tuple:
-                c_prev = state
-            else:
                 (c_prev, _) = state
+            else:
+                c_prev = state
             # Forget Gate
             f = tf.sigmoid(
                 tf.matmul(inputs, self.Wf) + self.bf
